7c4e132d90163f6210deda138d234c296409420f
Luca Zoppetti <lucazoppetti@outlook.com>
2024-02-27 16:40:13 +0100

    normalise audio files

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
-import librosa
-import sounddevice as sd
+import os
+from normalise import *
+import soundfile as sf
 
+AUDIO_FOLDER = "./audio"
 
-def process_audio_file(file_path: str, threshold: float):
-    audio, sr = librosa.load(file_path, sr=None)  # Load the audio file
-    audio_abs = abs(audio)  # Take the absolute values of the audio samples
+for file_name in os.listdir(AUDIO_FOLDER):
+    if file_name.endswith(".wav"):
+        file_path = os.path.join(AUDIO_FOLDER, file_name)
 
-    # Play the audio file
-    sd.play(audio, sr)
+        y, sr = librosa.load(file_path, sr=None)
+        y_normalised = normalise(y, -3)
 
-    for i in range(len(audio_abs)):
-        print(audio_abs[i])
-        # if audio_abs[i] >= threshold:
-        #     print(f"Threshold reached at sample {i}!")
-
-    # Wait for the audio file to finish playing
-    sd.wait()
-
-
-# Example usage
-file_path = "test.wav"
-threshold = 0.5  # Adjust the threshold value as per your requirement
-
-process_audio_file(file_path, threshold)
+        sf.write(file_path, y_normalised, sr)
